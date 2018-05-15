023c77820af295007fe5b65743b986e0bff95912
Bleuzen <supgesu@gmail.com>
2018-05-15 13:58:30 +0200

    better compile script

--- compile.sh
+++ compile.sh
@@ -1,1 +1,12 @@
-python -O -m py_compile spotifyrecorder.py
+# Without optimize
+#python -m py_compile spotifyrecorder.py
+
+# remove assert and __debug__-dependent statements; add .opt-1 before .pyc extension; also PYTHONOPTIMIZE=x
+#python -O -m py_compile spotifyrecorder.py
+
+# do -O changes and also discard docstrings; add .opt-2 before .pyc extension
+python -OO -m py_compile spotifyrecorder.py
+
+
+mv __pycache__/spotifyrecorder.cpython-36.opt-2.pyc .
+rm -r __pycache__/
