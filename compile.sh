c62b4e02b405c12531ac86b70ce4c540df902500
Bleuzen <supgesu@gmail.com>
2018-05-19 12:12:41 +0200

    rename part 2

--- compile.sh
+++ compile.sh
@@ -1,12 +1,5 @@
-# Without optimize
-#python -m py_compile spotifyrecorder.py
 
-# remove assert and __debug__-dependent statements; add .opt-1 before .pyc extension; also PYTHONOPTIMIZE=x
-#python -O -m py_compile spotifyrecorder.py
-
-# do -O changes and also discard docstrings; add .opt-2 before .pyc extension
-python -OO -m py_compile spotifyrecorder.py
-
+python -OO -m py_compile spotrec.py
 
 mv __pycache__/spotifyrecorder.cpython-36.opt-2.pyc .
 rm -r __pycache__/
