7f0cce62468bf6f5fc85464e210f6aa0142f32ce
Bleuzen <supgesu@gmail.com>
2018-09-28 19:28:58 +0200

    update compile script

--- compile.sh
+++ compile.sh
@@ -1,7 +1,7 @@
 
 python -OO -m py_compile spotrec.py
 
-mv __pycache__/spotrec.cpython-36.opt-2.pyc .
+mv __pycache__/spotrec.cpython-37.opt-2.pyc .
 rm -r __pycache__/
 
-echo "SHA1: $(sha1sum spotrec.cpython-36.opt-2.pyc)"
+echo "SHA256: $(sha256sum spotrec.cpython-37.opt-2.pyc)"
